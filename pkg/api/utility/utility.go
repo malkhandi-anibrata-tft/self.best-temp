f942074397127bd016879646fdac7f48827cbf47
Anibrata Malkhandi <malkhandi.anibrata@tftus.com>
2023-09-12 15:42:30 +0530

    srn: minor change in utility

--- pkg/api/utility/utility.go
+++ pkg/api/utility/utility.go
@@ -1,18 +1,7 @@
 package utility
 
-import (
-	"log"
-
-	"github.com/go-errors/errors"
-)
-
 func CreateErrorResponse(err error) map[string]string {
-	LogErrorWithStacktrace(err)
 	return map[string]string{
 		"Error": err.Error(),
 	}
-}
-
-func LogErrorWithStacktrace(err error) {
-	log.Println(err.(*errors.Error).ErrorStack())
 }
